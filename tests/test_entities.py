9b619fb0416cc9380a8b1b4dd923873a4cb2e9f9
Andrey Volobuev <avvolob@gmail.com>
2022-08-24 14:40:02 +0300

    Created first test case

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -1,9 +1,112 @@
-import pytest
-import entities
+import unittest
+from unittest.mock import MagicMock, patch
+from pycdek import CDEK
+from tests import data
+from uuid import UUID
 
 
-def test_currency():
-    assert entities.Currency.RUB == 1
-    assert entities.Currency(3) == entities.Currency.USD
-    assert entities.Currency.CNY == 6
-    assert entities.Currency(13) == entities.Currency.KRW
+class TestPycdek(unittest.TestCase):
+    @patch('pycdek.entities.AccessToken.is_valid', return_value=True)
+    def setUp(self, token_mock):
+        CLIENT_ID = '................................'
+        CLIENT_SECRET = '................................'
+        self.cdek = CDEK(CLIENT_ID, CLIENT_SECRET)
+
+
+    @patch('pycdek.entities.AccessToken.is_valid', return_value=True)
+    def test_create_order(self, token_mock):
+        with patch('requests.get') as session:
+            session.return_value = MagicMock(
+                status_code=200, 
+                json=MagicMock(
+                    return_value=data.cities_moscow
+                )
+            )
+
+            cities = self.cdek.get_cities(city="Москва")
+        
+        self.assertIn('44', [city.code for city in cities])
+        moscow = cities[0]
+        from_location = self.cdek.get_location("Тверская 1", moscow)
+        self.assertEqual(from_location.address, "Тверская 1")
+
+        with patch('requests.get') as session:
+            session.return_value = MagicMock(
+                status_code=200, 
+                json=MagicMock(
+                    return_value=data.cities_vladivostok
+                )
+            )
+
+            cities = self.cdek.get_cities(city="Владивосток")
+        
+        self.assertIn('288', [city.code for city in cities])
+        vladivostok = cities[1]
+        to_location = self.cdek.get_location("Светланская 1", vladivostok)
+        self.assertEqual(to_location.address, "Светланская 1")
+
+        package = self.cdek.create_package(name="Подарок", weight=1)
+        self.assertEqual(package.weight, 1)
+        self.assertIn('Подарок', [item.name for item in package.items])
+
+        with patch('requests.post') as session:
+            session.return_value = MagicMock(
+                status_code=200, 
+                json=MagicMock(
+                    return_value=data.tariffs
+                )
+            )
+
+            tariffs = self.cdek.get_available_tariffs(
+                from_location=from_location, to_location=to_location, packages=[package]
+            )
+
+        self.assertEqual(31, len(tariffs.tariffs))
+        self.assertEqual(696, tariffs.fastest.tariff_code)
+        self.assertEqual(234, tariffs.cheapest.tariff_code)
+        tariff = tariffs.fastest
+
+        sender = self.cdek.get_contact(name="Иванов Иван Иванович", phones=["+79111111111"])
+        self.assertEqual("Иванов Иван Иванович", sender.name)
+        self.assertIn('+79111111111', [phone.number for phone in sender.phones])
+
+        recipient = self.cdek.get_contact(name="Петров Петр Петрович", phones=["+79222222222"])
+        self.assertEqual("Петров Петр Петрович", recipient.name)
+        self.assertIn('+79222222222', [phone.number for phone in recipient.phones])
+
+        with patch('requests.post') as session:
+            session.return_value = MagicMock(
+                status_code=200, 
+                json=MagicMock(
+                    return_value=data.created_order2
+                )
+            )
+
+            order = self.cdek.register_order(
+                tariff=tariff,
+                from_location=from_location,
+                to_location=to_location,
+                packages=[package],
+                sender=sender,
+                recipient=recipient
+            )
+
+        self.assertEqual(UUID("72753034-cc78-4472-8e7f-8cebdc04ff86"), order.entity.uuid)
+        self.assertEqual("CREATE", order.requests[0].type)
+        self.assertEqual("ACCEPTED", order.requests[0].state)
+
+        with patch('requests.get') as session:
+            session.return_value = MagicMock(
+                status_code=200, 
+                json=MagicMock(
+                    return_value=data.order_info2
+                )
+            )
+
+            info = self.cdek.get_order_info(order.entity.uuid)
+
+        self.assertEqual(UUID("72753034-cc78-4472-8e7f-8cebdc04ff86"), info.entity.uuid)
+        self.assertEqual(1, info.entity.type)
+        self.assertEqual(tariff.tariff_code, info.entity.tariff_code)
+
+        
