d4467eb433d52a1e5925effb211d1f4f38bd6838
Andrey Volobuev <avvolob@gmail.com>
2022-09-06 22:58:42 +0300

    Made tests to work with async client

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -1,13 +1,13 @@
-import unittest
-from unittest.mock import MagicMock, patch
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import MagicMock, patch, AsyncMock
 from pycdek import CDEK, entities
 from tests import data
 from uuid import UUID
 
 
-class TestPycdek(unittest.TestCase):
+class TestPycdek(IsolatedAsyncioTestCase):
     @patch(
-        "pycdek.auth.TokenManager._read_token_from_file",
+        "pycdek.client.TokenManager._read_token_from_file",
         return_value=entities.AccessToken(
             access_token="....", expires_in="6000", scope="...", jti="...."
         ),
@@ -18,32 +18,32 @@ class TestPycdek(unittest.TestCase):
         self.cdek = CDEK(CLIENT_ID, CLIENT_SECRET)
 
     @patch(
-        "pycdek.auth.TokenManager._read_token_from_file",
+        "pycdek.client.TokenManager._read_token_from_file",
         return_value=entities.AccessToken(
             access_token="....", expires_in="6000", scope="...", jti="...."
         ),
     )
-    def test_create_order(self, token_mock):
-        with patch("requests.get") as session:
-            session.return_value = MagicMock(
-                status_code=200, json=MagicMock(return_value=data.cities_moscow)
-            )
+    @patch("aiohttp.ClientSession.post")
+    @patch("aiohttp.ClientSession.get")
+    async def test_create_order(self, get_session, post_session, token_mock):
+        r = MagicMock(status=200)
+        r.json = AsyncMock(return_value=data.cities_moscow)
+        context_manager = MagicMock()
+        context_manager.__aenter__.return_value = r
+        get_session.return_value = context_manager
+        post_session.return_value = context_manager
 
-            cities = self.cdek.get_cities(city="Москва")
-
+        cities = await self.cdek.get_cities(city="Москва")
         self.assertIn("44", [city.code for city in cities])
+
         moscow = cities[0]
         from_location = self.cdek.get_location("Тверская 1", moscow)
         self.assertEqual(from_location.address, "Тверская 1")
 
-        with patch("requests.get") as session:
-            session.return_value = MagicMock(
-                status_code=200, json=MagicMock(return_value=data.cities_vladivostok)
-            )
-
-            cities = self.cdek.get_cities(city="Владивосток")
-
+        r.json = AsyncMock(return_value=data.cities_vladivostok)
+        cities = await self.cdek.get_cities(city="Владивосток")
         self.assertIn("288", [city.code for city in cities])
+        
         vladivostok = cities[1]
         to_location = self.cdek.get_location("Светланская 1", vladivostok)
         self.assertEqual(to_location.address, "Светланская 1")
@@ -52,15 +52,10 @@ class TestPycdek(unittest.TestCase):
         self.assertEqual(package.weight, 1)
         self.assertIn("Подарок", [item.name for item in package.items])
 
-        with patch("requests.post") as session:
-            session.return_value = MagicMock(
-                status_code=200, json=MagicMock(return_value=data.tariffs)
-            )
-
-            tariffs = self.cdek.get_available_tariffs(
-                from_location=from_location, to_location=to_location, packages=[package]
-            )
-
+        r.json = AsyncMock(return_value=data.tariffs)
+        tariffs = await self.cdek.get_available_tariffs(
+            from_location=from_location, to_location=to_location, packages=[package]
+        )
         self.assertEqual(31, len(tariffs.tariffs))
         self.assertEqual(696, tariffs.fastest.tariff_code)
         self.assertEqual(234, tariffs.cheapest.tariff_code)
@@ -77,20 +72,16 @@ class TestPycdek(unittest.TestCase):
         )
         self.assertEqual("Петров Петр Петрович", recipient.name)
         self.assertIn("+79222222222", [phone.number for phone in recipient.phones])
-
-        with patch("requests.post") as session:
-            session.return_value = MagicMock(
-                status_code=200, json=MagicMock(return_value=data.created_order2)
-            )
-
-            order = self.cdek.register_order(
-                tariff=tariff,
-                from_location=from_location,
-                to_location=to_location,
-                packages=[package],
-                sender=sender,
-                recipient=recipient,
-            )
+        
+        r.json = AsyncMock(return_value=data.created_order2)
+        order = await self.cdek.register_order(
+            tariff=tariff,
+            from_location=from_location,
+            to_location=to_location,
+            packages=[package],
+            sender=sender,
+            recipient=recipient,
+        )
 
         self.assertEqual(
             UUID("72753034-cc78-4472-8e7f-8cebdc04ff86"), order.entity.uuid
@@ -98,12 +89,8 @@ class TestPycdek(unittest.TestCase):
         self.assertEqual("CREATE", order.requests[0].type)
         self.assertEqual("ACCEPTED", order.requests[0].state)
 
-        with patch("requests.get") as session:
-            session.return_value = MagicMock(
-                status_code=200, json=MagicMock(return_value=data.order_info2)
-            )
-
-            info = self.cdek.get_order_info(order.entity.uuid)
+        r.json = AsyncMock(return_value=data.order_info2)
+        info = await self.cdek.get_order_info(order.entity.uuid)
 
         self.assertEqual(UUID("72753034-cc78-4472-8e7f-8cebdc04ff86"), info.entity.uuid)
         self.assertEqual(1, info.entity.type)
